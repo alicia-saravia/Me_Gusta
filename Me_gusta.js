6d7a5ab5085a8e42d43c25f6ccb674d7fddd1fbe
Alicia Saravia <alicia.saraviam@gmail.com>
2022-08-28 13:36:02 -0400

    Tarea Me Gusta

---
+++ Me_gusta.js
@@ -0,0 +1,20 @@
+function like_neil(){
+    contador = document.querySelector(".conteiner-principal .algunos-usuarios #like_neil");
+    valor = parseInt(contador.innerHTML);
+    valor = valor + 1;
+    contador.innerHTML = valor;
+}
+
+function like_nichole(){
+    contador = document.querySelector(".conteiner-principal .algunos-usuarios #like_nichole");
+    valor = parseInt(contador.innerHTML);
+    valor = valor + 1;
+    contador.innerHTML = valor;
+}
+
+function like_jim(){
+    contador = document.querySelector(".conteiner-principal .algunos-usuarios #like_jim");
+    valor = parseInt(contador.innerHTML);
+    valor = valor + 1;
+    contador.innerHTML = valor;
+}
